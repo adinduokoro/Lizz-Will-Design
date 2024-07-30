f54a85db5830fa73fb66fb8796a6ea389a83bebb
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-07-30 17:12:51 -0400

    Working on the CTA section.

--- src/components/cta/Cta.js
+++ src/components/cta/Cta.js
@@ -1,12 +1,25 @@
 import React from "react";
 import styles from "./Cta.module.css";
-import background from "../../assets/cta-background.png";
+import SectionLink from "../sectionLink/SectionLink";
 
 const Cta = () => {
   return (
     <div className={styles["cta"]}>
-      <img src={background} alt="" />
-
+      <div className={styles["parallax"]}></div>
+      <div className={styles["top"]}>
+        <div className={styles["top-container"]}>
+          <div className={styles["link-container"]}>
+            <SectionLink text={"Contact Us"} />
+          </div>
+          <h2>Get in Touch</h2>
+          <p>Remodeling & Design, Tailored to Your Vision</p>
+        </div>
+      </div>
+      <div className={styles["bottom"]}>
+        <h2>Thinking about a new project?</h2>
+        <p>Feel free to say Hello...</p>
+        <button>Let's Talk</button>
+      </div>
     </div>
   );
 };
