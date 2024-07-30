914b860c2a830170214b7cd7c6cd7b06e1bc5574
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-07-30 09:18:19 -0400

    Service Section
    Background has been applied. Working on the css of the section.

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,10 +1,40 @@
-import React from 'react'
-import styles from "./Service.module.css"
+import React from "react";
+import styles from "./Service.module.css";
+import { cards } from "./data";
+import backgroundImage from "../../assets/homeBackgroundTwo.png"
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <div className={styles["service"]}>
+      <div className={styles["background-container"]}>
+        <div className={styles["background-overlay"]}></div>
+        <img src={backgroundImage} alt="background"/>
+      </div>
+      <div className={styles["service-container"]}>
+        <h2>Explore Our Key Services and More</h2>
+        <div className={styles["service-content"]}>
+          <div className={styles["description"]}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+              Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+              Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris
+              massa. Vestiem at nibh elementum imperdiet. Duis sagittis ipsum.{" "}
+            </p>
+            <button className="primary-button-one">SEE SERVICES</button>
+          </div>
+          <div className={styles["cards"]}>
+            {cards.map((card, index) => (
+              <div className={styles["card"]} key={index}>
+                <div className={styles["top"]}>{card.img}top</div>
+                <div className={styles["bottom"]}>{card.title}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Services
+export default Services;
---
+++ src/components/services/data.js
@@ -0,0 +1,18 @@
+export const cards = [
+  {
+    img: "",
+    title: "General Contracting",
+  },
+  {
+    img: "",
+    title: "Handyman Service",
+  },
+  {
+    img: "",
+    title: "Painting & Drywall",
+  },
+  {
+    img: "",
+    title: "Kitchen & Bathroom",
+  },
+];
