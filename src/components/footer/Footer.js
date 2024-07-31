4eea0b7218fc1dd495bcc14daeccef91cc828084
Addie Okoro <adinduokoro95@gmail.com>
2024-07-31 00:52:56 -0400

    Footer finished.
    Finished most of the footer which means  most of the responsiveness at 1920x1080 is finished. Working on the tablet next.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,21 +1,67 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import { contactDetails, socialLinks } from "./data";
+import logo from "../../assets/clean-logo.svg";
 
 const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+
   return (
-    <div className={styles["footer"]}>
+    <footer className={styles["footer"]}>
       <img src="" alt="" />
       <div className={styles["footer-container"]}>
         <div className={styles["top"]}>
-          <div className={styles["left"]}>{/* company details */}</div>
-          <div className={styles["right"]}>{/* form */}</div>
+          <div className={styles["left"]}>
+            <img src={logo} alt="" />
+            <div className={styles["address"]}>
+              <span>PO Box 123</span>
+              <span>Baltimore, MD 21236</span>
+            </div>
+            <div className={styles["contacts"]}>
+              {contactDetails.map((contact, index) => (
+                <div className={styles["contact"]} key={index}>
+                  <img src={contact.img} alt={contact.name} />
+                  <span>{contact.detail}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+          <form className={styles["right"]}>
+            <div className={styles["colOne"]}>
+              <span>Let’s get started! Complete the following form:</span>
+              <input
+                className={styles["name"]}
+                type="name"
+                placeholder="Name"
+              />
+              <input
+                className={styles["email"]}
+                type="email"
+                placeholder="Email"
+              />
+              <button>SUBMIT</button>
+            </div>
+            <div className={styles["colTwo"]}>
+              <textarea type="text" placeholder="How can we help you?" />
+            </div>
+          </form>
         </div>
         <div className={styles["bottom"]}>
-          {/* copyright */}
-          {/* social */}
+          <div className={styles["copyright"]}>
+            © {year} Lizz Will Design & Renovation. All rights reserved. Created
+            by <span>Byte Back Digital.</span>
+          </div>
+          <div className={styles["social-media-icons"]}>
+            {socialLinks.map((social, index) => (
+              <div className={styles["social-icon"]} key={index}>
+                <img src={social.img} alt={social.name} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
---
+++ src/components/footer/data.js
@@ -0,0 +1,20 @@
+import linkedIn from "../../assets/ri_linkedin-fill.svg";
+import twitter from "../../assets/mdi_twitter.svg";
+import facebook from "../../assets/ri_facebook-fill.svg";
+import instagram from "../../assets/mdi_instagram.svg";
+import license from "../../assets/mdi_card-account-details.svg";
+import phone from "../../assets/mdi_phone.svg";
+import email from "../../assets/mdi_email.svg";
+
+export const socialLinks = [
+  { name: "linkedin", img: linkedIn },
+  { name: "twitter", img: twitter },
+  { name: "facebook", img: facebook },
+  { name: "instagram", img: instagram },
+];
+
+export const contactDetails = [
+  { name: "license", img: license, detail: "MHIC 01-157071" },
+  { name: "phone", img: phone, detail: "443-218-3344" },
+  { name: "email", img: email, detail: "lizwilldesign@gmail.com" },
+];
