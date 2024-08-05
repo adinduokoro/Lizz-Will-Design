1c520918ca4734fdaf273373b4696287e591e6d9
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-08-05 13:06:53 -0400

    hosting

--- src/assets/background/homePageBackgroundOne/HomePageBackGroundOne.js
+++ src/assets/background/homePageBackgroundOne/HomePageBackGroundOne.js
@@ -1,12 +1,19 @@
 import React from "react";
-import styles from "./HomePageBackGroundOne.module.css"
+import styles from "./HomePageBackGroundOne.module.css";
 
-const HomePageBackGroundOne = ({video}) => {
+const HomePageBackGroundOne = ({ video }) => {
   return (
     <div className={styles["home-page-background-one"]}>
       <div className={styles["video-container"]}>
         <div className={styles["background-one-overlay"]}></div>
-        <video className={styles["video"]} autoPlay muted loop>
+        <video
+          className={styles["video"]}
+          autoPlay
+          muted
+          loop
+          playsInline
+          preload="auto"
+        >
           <source src={video} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
