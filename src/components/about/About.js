2962e4c49cca4f1a0a0bdea3af1d87979320b3ce
Addie Okoro <adinduokoro95@gmail.com>
2024-08-07 22:42:03 -0400

    About section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -23,6 +23,7 @@ const About = () => {
               ))}
             </ul>
             <p>
+              {/* 590 character count */}
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
               nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
               Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./Hero.module.css";
+import Typed from "typed.js";
 
 const Hero = () => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Dream", "Vision", "Imagination", "Aspirations", "Personality"],
+      typeSpeed: 200,
+      backSpeed: 75,
+      backDelay: 2700,
+      loop: true,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div className={styles["hero"]}>
       <div className={styles["hero-container"]}>
         <div className={styles["hero-cta"]}>
           <h1>
-            Designing homes to perfectly match your <span>Dreams.</span>
+            Designing homes to perfectly match your <span ref={el}></span>
           </h1>
           <p>
             General Contracting, Handyman Services, Painting & Drywall, Kitchen
