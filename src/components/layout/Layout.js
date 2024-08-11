629a860e0c550f678e569c58123dc8173f1838cd
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-08-11 08:38:01 -0400

    Mobile menu updates

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -6,6 +6,7 @@ import video from "../../assets/hero-video.mp4";
 import HomePageBackGroundOne from "../../assets/background/homePageBackgroundOne/HomePageBackGroundOne";
 import closeIcon from "../../assets/close-icon.svg";
 import headerIcon from "../../assets/header-img-icon.svg";
+import { socialLinks } from "./data";
 
 const Layout = () => {
   const location = useLocation();
@@ -21,10 +22,10 @@ const Layout = () => {
             <img src={headerIcon} alt="header icon" />
             <h3>About Us</h3>
           </div>
-          <div className={styles["about-image-container"]}>
-            img
-          </div>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          <div className={styles["about-image-container"]}>img</div>
+          <p>
+            Lorem Ipsum is simply dummy text of tting and typesetting industry.
+          </p>
           <button className="primary-button-one">Learn More</button>
           <div className={styles["mobile-header"]}>
             <img src={headerIcon} alt="header icon" />
@@ -33,12 +34,22 @@ const Layout = () => {
           {/* icon address */}
           {/* icon phone */}
           {/* icon email */}
-          {/* social icons */}
-          <div className={styles["image-container"]}>
-            img
+          <div className={styles["social-icons"]}>
+            {socialLinks.map((icon, index) => (
+              <div className={styles["icon"]} key={index}>
+                <img src={icon.img} alt={icon.name} />
+              </div>
+            ))}
           </div>
-          {/* img header */}
-          {/* img description */}
+          <div className={styles["image-container"]}>img</div>
+          <div className={styles["image-description"]}>
+            <h3>Do You Need Any Help?</h3>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing Lorem Ipsum is
+              Lorem Ipsum is Lorem Ipsum is Lorem Ipsum is and typesetting
+              industry the printing .
+            </p>
+          </div>
         </div>
       </div>
       {location.pathname === "/" && <HomePageBackGroundOne video={video} />}
---
+++ src/components/layout/data.js
@@ -0,0 +1,11 @@
+import linkedIn from "../../assets/ri_linkedin-fill-orange.svg";
+import twitter from "../../assets/mdi_twitter-orange.svg";
+import facebook from "../../assets/ri_facebook-fill-orange.svg";
+import instagram from "../../assets/mdi_instagram-orange.svg";
+
+export const socialLinks = [
+    { name: "linkedin", img: linkedIn, href: "https://www.linkedin.com/" },
+    { name: "twitter", img: twitter, href: "https://twitter.com/?lang=en" },
+    { name: "facebook", img: facebook, href: "https://www.facebook.com/" },
+    { name: "instagram", img: instagram, href: "https://www.instagram.com/" },
+  ];
