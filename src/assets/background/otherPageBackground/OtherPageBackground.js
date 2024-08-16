3dad2e3773f554a7a6e05472ac58a76178b100b7
Addie Okoro <adinduokoro95@gmail.com>
2024-08-15 20:34:32 -0400

    Background

---
+++ src/assets/background/otherPageBackground/OtherPageBackground.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./OtherPageBackground.module.css";
+
+const OtherPageBackground = () => {
+  return (
+    <div className={styles["other-page-background"]}>
+      <div className={styles["other-page-background-container"]}>
+        Hello World
+      </div>
+    </div>
+  );
+};
+
+export default OtherPageBackground;
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -4,7 +4,7 @@ import { Navigation, Footer, Menu } from "../../components";
 import { Outlet, useLocation } from "react-router-dom";
 import video from "../../assets/hero-video.mp4";
 import HomePageBackGroundOne from "../../assets/background/homePageBackgroundOne/HomePageBackGroundOne";
-
+import OtherPageBackground from "../../assets/background/otherPageBackground/OtherPageBackground";
 
 const Layout = () => {
   const location = useLocation();
@@ -12,7 +12,7 @@ const Layout = () => {
   return (
     <div className={styles.layout}>
       <Menu />
-      {location.pathname === "/" && <HomePageBackGroundOne video={video} />}
+      {location.pathname === "/" ? <HomePageBackGroundOne video={video} /> : <OtherPageBackground />}
       <Navigation />
       <Outlet />
       <Footer />
